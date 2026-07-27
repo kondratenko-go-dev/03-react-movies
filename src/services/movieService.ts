ce8b319ac31d220c0766fb3b66af655b680de41b
kondratenko-go-dev <kondratenko.aleksandr.go.dev@gmail.com>
2026-07-27 14:25:45 +0300

    03-react-movies-update

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,5 +1,12 @@
 import axios from 'axios';
-import type { Movie, MoviesResponse } from '../types/movie';
+import type { Movie } from '../types/movie';
+
+export interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
